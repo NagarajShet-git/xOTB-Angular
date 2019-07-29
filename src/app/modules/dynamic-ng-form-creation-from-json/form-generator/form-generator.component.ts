08b216c7589a1a3d3b554b7db3f8357143b11d21
vijay kumar navulla <vijaykumarnavulla@gmail.com>
2019-07-29 15:59:51 +0530

    added the dynamic row count for form generator components.

--- src/app/modules/dynamic-ng-form-creation-from-json/form-generator/form-generator.component.ts
+++ src/app/modules/dynamic-ng-form-creation-from-json/form-generator/form-generator.component.ts
@@ -3,39 +3,75 @@ import { MatCheckbox } from '@angular/material';
 
 @Component({
   selector: "app-form-generator",
-  templateUrl: "./form-generator.component.html",
+  templateUrl: "./form-generator-tmp.component.html",
   styleUrls: ["./form-generator.component.css"]
 })
 export class FormGeneratorComponent implements OnInit {
   @Input() jsonFormData;
   @Input() formGeneratorName;
+  // @Input() rowCount:number = 3;
+
+  public fxPercent: number=100;
 
   @Output() btnEvent = new EventEmitter<object>();
+  @Input() set rowCount(value: number) {
+    switch (value) {
+      case 1:
+        this.fxPercent = 100;
+        break;
+      case 2:
+        this.fxPercent = 48;
+        break;
+
+      case 3:
+        this.fxPercent = 30;
+        break;
+
+      case 4:
+        this.fxPercent = 22;
+        break;
+
+      // case 5:
+      //   this.fxPercent = 8;
+      //   break;
+      default:
+        this.fxPercent = 100;
+    }
+  }
+
 
   public selected: string;
-  @ViewChild('ngForm') ngForm: any;
-  constructor() { }
+@ViewChild('ngForm') ngForm: any;
+constructor() { }
 
-  ngOnInit() { }
+ngOnInit() {
+  // this.fxPercent = 22;
+}
 
 
-  chechType(data) {
-    return (
-      data.type == "text" ||
-      data.type == "password" ||
-      data.type == "number" ||
-      data.type == "tel" ||
-      data.type == "email" ||
-      data.type == "date"
-    );
-  }
+chechType(data) {
+  return (
+    data.type == "text" ||
+    data.type == "password" ||
+    data.type == "number" ||
+    data.type == "tel" ||
+    data.type == "email" ||
+    data.type == "date"
+  );
+}
 
-  onButtonClick(btnObj) {
-    let obj = {
-      name: btnObj.name,
-      value: btnObj.value,
-      updatedJsonFormData: this.jsonFormData
-    };
-    this.btnEvent.emit(obj);
-  }
+onButtonClick(btnObj) {
+  let obj = {
+    name: btnObj.name,
+    value: btnObj.value,
+    updatedJsonFormData: this.jsonFormData
+  };
+  this.btnEvent.emit(obj);
+}
+
+calculate(w){
+  let per;
+  per = w / 3;
+  // per =
+}
 }
