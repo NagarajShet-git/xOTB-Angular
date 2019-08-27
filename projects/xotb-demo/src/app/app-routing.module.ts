6a6c828424b760b5cfb351d240e934bbcf128f53
yelendisureshbabu-hcl <yelendisuresh.babu@hcl.com>
2019-08-27 09:47:12 +0530

    accordion

--- projects/xotb-demo/src/app/app-routing.module.ts
+++ projects/xotb-demo/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { DemoInputComponent } from './examples/input/input.component';
 import { DemoTooltipsBasic } from './examples/tooltips/tooltips.component';
 import { DemoSelectComponent } from './examples/select/select.component';
 import { DemoCheckboxesComponent } from './examples/checkboxes/checkboxes.component';
+import { DemoAccordinComponent } from './examples/accordion/accordion.component';
 
 const routes: Routes = [
   { path: 'input', component: DemoInputComponent },
   { path: 'tooltip', component: DemoTooltipsBasic },
   { path: 'select', component: DemoSelectComponent },
-  { path: 'checkboxes', component: DemoCheckboxesComponent }
+  { path: 'checkboxes', component: DemoCheckboxesComponent },
+  { path: 'accordion', component: DemoAccordinComponent }
 ];
 
 @NgModule({
