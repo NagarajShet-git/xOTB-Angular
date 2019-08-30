e593109ae591020cacc55c05b038fa9337847ccb
akuntal <akuntal@gmail.com>
2019-08-30 23:07:32 +0530

    spinner css

--- projects/spinner/src/lib/spinner.module.ts
+++ projects/spinner/src/lib/spinner.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { XotbSpinner } from './spinner';
+import { XotbThemeModule } from 'ng-xotb/xotb-theme';
 
 @NgModule({
   declarations: [XotbSpinner],
-  imports: [CommonModule],
+  imports: [CommonModule, XotbThemeModule],
   exports: [XotbSpinner]
 })
 export class XotbSpinnerModule {}
