8029ef206ef3384bdf290f2bcde52f12139a15a7
akuntal <akuntal@gmail.com>
2019-09-12 16:11:09 +0530

    css

--- projects/xotb-theme/src/lib/xotb.theme-light.ts
+++ projects/xotb-theme/src/lib/xotb.theme-light.ts
@@ -6,65 +6,38 @@ export const theme = {
   'xotb_common__background-color': '#ffffff',
   'xotb_common__disabled__border-color': '#c9c7c5',
   'xotb_common__disabled__background-color': '#ecebea',
-
-  // Input
-  'xotb-input__background-color': '#fff',
-  'xotb-input__border-color': '#dddbda',
+  'xotb-common__box-shadow': 'rgba(0, 0, 0, 0.16)',
 
   // Form
   'xotb-form-element__label__color': '#3e3e3c',
 
   // tooltips
-  'xotb-popover__background-color': '#fff',
-  'xotb-popover__box-shadow': '0 2px 3px 0 rgba(0, 0, 0, 0.16)',
-  'xotb-popover__border-color': '#dddbda',
   'xotb-popover_tooltip__background': '#16325c',
-  'xotb-popover__body__color': '#fff',
 
   // select
   'xotb-select_container__border-color': '#061c3f',
-  'xotb-select__background-color': '#fff',
-  'xotb-select__border-color': '#dddbda',
-
-  // form field error
-  'xotb-has-error__color': '#c23934',
-  'xotb-has-error__border-color': '#c23934',
-  'xotb-has-error__background-color': '#fff',
-  'xotb-has-error__box-shadow-color': '#c23934',
-
-  // form field required
-  'xotb-required__color': '#c23934',
-
-  // accordion
-  'xotb-accordion__list-item__border-top-color': '#dddbda',
 
   // buttons
-  'xotb-button__color': '#0070d2',
-  'xotb-button_inverse__border-color': '#dddbda',
-  'xotb-button_neutral__border-color': '#dddbda',
-  'xotb-button_neutral__background-color': '#ffffff',
-  'xotb-button_brand__border-color': '#0070d2',
-  'xotb-button_brand__background-color': '#0070d2',
   'xotb-button_brand__color': '#ffffff',
-  'xotb-button_outline-brand__border-color': '#dddbda',
-  'xotb-button_outline-brand__background-color': '#ffffff',
-  'xotb-button_destructive__border-color': '#c23934',
-  'xotb-button_destructive__background-color': '#c23934',
   'xotb-button_destructive__color': '#ffffff',
-  'xotb-button_text-destructive__border-color': '#dddbda',
-  'xotb-button_text-destructive__background-color': '#ffffff',
-  'xotb-button_text-destructive__color': '#c23934',
   'xotb-button_success__border-color': '#4bca81',
   'xotb-button_success__background-color': '#4bca81',
   'xotb-button_success__color': '#080707',
 
   // checkbox
   'xotb-checkbox__disabled__after__border-color': '#969492',
-  'xotb-checkbox__disabled__background-color': '#ecebea',
-  'xotb-checkbox__disabled__border-color': '#c9c7c5',
-  'xotb-checkbox__checked__border-bottom-color': '#0070d2',
-  'xotb-checkbox__checked__border-left-color': '#0070d2',
 
   // progress-bar
-  'xotb-progress-bar__value__background-color': '#5eb4ff'
+  'xotb-progress-bar__value__background-color': '#5eb4ff',
+
+  // toast
+  'xotb-notify_toast__color': '#ffffff',
+  'xotb-notify_toast__background': '#706e6b',
+  'xotb-theme_info__background': '#706e6b',
+  'xotb-theme_success__background': '#04844b',
+  'xotb-theme_warning__background': '#ffb75d',
+  'xotb-theme_warning__color': '#080707',
+
+  // icons
+  'xotb-icon__fill': '#ffffff'
 };
