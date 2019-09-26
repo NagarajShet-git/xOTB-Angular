590fbe722eb07446da98f090f9287b9e3fc0740a
NagarajShet-git <rajushet91@gmail.com>
2019-09-26 16:10:32 +0530

    card vbox and hbox changes

--- projects/xotb-demo/src/app/examples/hbox/hbox.component.ts
+++ projects/xotb-demo/src/app/examples/hbox/hbox.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './hbox.component.html'
+  templateUrl: './hbox.component.html',
+  styleUrls: ['./hbox.component.scss']
 })
 export class DemoHBoxComponent {}
--- projects/xotb-demo/src/app/examples/vbox/vbox.component.ts
+++ projects/xotb-demo/src/app/examples/vbox/vbox.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './vbox.component.html'
+  templateUrl: './vbox.component.html',
+  styleUrls: ['./vbox.component.scss']
 })
 export class DemoVBoxComponent {}
