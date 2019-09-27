37146a2bd516a4a70743b0fe5719f2cf79e0ed79
Alok <hello@ghbot.com>
2019-09-27 17:59:31 +0530

    stories

--- src/stories/knowledge/welcome/welcome.stories.ts
+++ src/stories/knowledge/welcome/welcome.stories.ts
@@ -1,5 +1,4 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
-import { centered } from '@storybook/addon-centered/angular';
 import { XotbPanelModule } from 'ng-xotb/containers/panel';
 
 storiesOf('The KNOWLEDGE|Welcome', module)
@@ -8,11 +7,15 @@ storiesOf('The KNOWLEDGE|Welcome', module)
       imports: [XotbPanelModule]
     })
   )
-  .addDecorator(centered)
   .add(
     'About',
     () => ({
-      template: '<p>...to be constructed</p>'
+      template: `
+        <div class="xotb-view-container">
+          <h2 class="heading">About</h2>
+          <p>The Clarity consists of predefined variables for basic design elements such as colour, typography, and iconography. With clearly authenticated guidelines and governance for design and content creation, we spend less time talking the guidelines to bring clarity for ourselves and more time creating designs and content that clearly serves our users.</p>
+        </div>
+        `
     }),
     {
       options: {
